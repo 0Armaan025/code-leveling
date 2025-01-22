3f79274ee3a5826790e617e946843ecca6fbb0e5
Armaan <armaan33000@gmail.com>
2025-01-22 17:55:22 +0530

    features done

--- src/components/feature-card/FeatureCard.tsx
+++ src/components/feature-card/FeatureCard.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 type Props = {
     logoUrl: string;
-    
+    featureTitle: string;
+    featureContent: string;
 }
 
 const FeatureCard = (props: Props) => {
-  return (
-    <>
-        <div className="featureCard"></div>
-    </>
-  );
+    return (
+        <div className="featureCard  sm:h-32 h-auto sm:w-auto w-full hover:cursor-pointer  py-4 px-4 rounded-xl flex flex-col bg-gradient-to-r from-[#18181c] via-[#090212] to-[#080112] bg-opacity-40 backdrop-blur-[0.2px] border border-transparent shadow-[inset_0_4px_6px_rgba(0,0,0,0.3)] hover:shadow-[0_6px_12px_rgba(0,0,0,0.2)] transition-all ease-in-out duration-200 justify-between items-start">
+
+            {/* Top section */}
+            <div className="flex flex-row justify-start items-center">
+                <div className="bg-[#2a0f45] rounded-full w-10 h-10 p-2">
+                    <Image src={props.logoUrl} alt="feature logo" width={80} height={80} />
+                </div>
+                <h3 className="text-[#fff] ml-2 font-semibold" style={{ fontFamily: "Montserrat, serif" }}>
+                    {props.featureTitle}
+                </h3>
+            </div>
+
+            {/* Content section */}
+            <h3 className="mt-4 text-[#747475] w-60 sm:w-auto mb-3 text-sm sm:text-base">
+                {props.featureContent}
+            </h3>
+
+        </div>
+    );
 }
 
-export default FeatureCard
+export default FeatureCard;
--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -6,63 +6,76 @@ import ElixirOfLife from '../elixir/elixir_of_life';
 import XCard from '../x-card/XCard';
 import ThemeToggleBtn from '../theme-toggle/ThemeToggleBtn';
 import CodeLevelingLogo from '../code-leveling-logo/CodeLevelingLogo';
+import FeatureCard from '../feature-card/FeatureCard';
 
 type Props = {}
 
 const HeroSection = (props: Props) => {
     return (
-        <div className="heroSection flex flex-col md:flex-row justify-start items-start md:items-start">
-            {/* Left Section */}
-            <div className="flex flex-col p-4 mt-16 justify-start ml-9 items-start md:items-start">
-                <h3
-                    className="text-4xl md:text-6xl text-white font-semibold md:w-[40rem] leading-snug md:leading-[4rem]"
-                    style={{ fontFamily: "Montserrat, serif" }}
-                >
-                    Get <span className="text-[#DF98FB]">
-                        <ScribbleText>coding boost</ScribbleText>
-                    </span> to turn yourself into a coding machine!
-                </h3>
+        <>
+            <div className="heroSection flex flex-col md:flex-row justify-start items-start md:items-start">
+                {/* Left Section */}
+                <div className="flex flex-col p-4 mt-16 justify-start ml-9 items-start md:items-start">
+                    <h3
+                        className="text-4xl md:text-6xl text-white font-semibold md:w-[40rem] leading-snug md:leading-[4rem]"
+                        style={{ fontFamily: "Montserrat, serif" }}
+                    >
+                        Get <span className="text-[#DF98FB]">
+                            <ScribbleText>coding boost</ScribbleText>
+                        </span> to turn yourself into a coding machine!
+                    </h3>
 
-                {/* Spacing */}
-                <div className="h-4 md:h-8"></div>
+                    {/* Spacing */}
+                    <div className="h-4 md:h-8"></div>
 
-                <h4
-                    className="text-lg md:text-xl text-[#a1a1aa] w-full md:w-[40rem]"
-                    style={{ fontFamily: "Hind Madurai" }}
-                >
-                    You get to level-up like never before! Motivation and dopamine at its peaks.
-                </h4>
+                    <h4
+                        className="text-lg md:text-xl text-[#a1a1aa] w-full md:w-[40rem]"
+                        style={{ fontFamily: "Hind Madurai" }}
+                    >
+                        You get to level-up like never before! Motivation and dopamine at its peaks.
+                    </h4>
 
-                {/* Spacing */}
-                <div className="h-4 md:h-8"></div>
+                    {/* Spacing */}
+                    <div className="h-4 md:h-8"></div>
 
-                {/* Buttons */}
-                <div className="flex flex-col md:flex-row justify-start items-center space-y-4 md:space-y-0">
-                    <input
-                        type="button"
-                        value="Get VS Code extension"
-                        className="px-5 py-2 w-60 bg-[#0064d7] text-white hover:bg-[#025cc4] transition-all cursor-pointer rounded-3xl text-lg font-medium"
-                        style={{ fontFamily: 'Montserrat, serif' }}
-                    />
-                    <input
-                        type="button"
-                        value="See documentation..."
-                        className="mt-4 w-60 md:mt-0 md:ml-4 px-5 py-2 bg-white bg-opacity-10 text-white hover:bg-opacity-15 transition-all cursor-pointer rounded-3xl text-lg font-medium"
-                        style={{ fontFamily: 'Montserrat, serif' }}
-                    />
+                    {/* Buttons */}
+                    <div className="flex flex-col md:flex-row justify-start items-center space-y-4 md:space-y-0">
+                        <input
+                            type="button"
+                            value="Get VS Code extension"
+                            className="px-5 py-2 w-60 bg-[#0064d7] text-white hover:bg-[#025cc4] transition-all cursor-pointer rounded-3xl text-lg font-medium"
+                            style={{ fontFamily: 'Montserrat, serif' }}
+                        />
+                        <input
+                            type="button"
+                            value="See documentation..."
+                            className="mt-4 w-60 md:mt-0 md:ml-4 px-5 py-2 bg-white bg-opacity-10 text-white hover:bg-opacity-15 transition-all cursor-pointer rounded-3xl text-lg font-medium"
+                            style={{ fontFamily: 'Montserrat, serif' }}
+                        />
+                    </div>
                 </div>
+
+                {/* Right Section */}
+                <div className="hidden md:flex flex-col ml-8 justify-center items-center relative">
+                    <div className="elixirDiv right-20 mt-8">
+                        <XCard />
+                        <ElixirOfLife />
+                    </div>
+                    <ThemeToggleBtn />
+                    <CodeLevelingLogo />
+                </div>
+
+
+
+            </div>
+            <div className="flex flex-col mb-8 sm:flex-row justify-start ml-8 items-center sm:space-x-4 space-y-8 sm:space-y-0">
+                <FeatureCard logoUrl="https://cdn-icons-png.flaticon.com/128/1067/1067357.png" featureContent='Get a great boost in your coding journey! ' featureTitle='Coding boost!' />
+                <FeatureCard logoUrl="https://cdn-icons-png.flaticon.com/128/12663/12663290.png" featureContent='Analyse the time spent on projects and tech-stacks you use!' featureTitle='Project insights!' />
+                <FeatureCard logoUrl="https://cdn-icons-png.flaticon.com/128/2817/2817958.png" featureContent='Level up and be on the leaderboard!' featureTitle='Ranking system!' />
+                <FeatureCard logoUrl="https://cdn-icons-png.flaticon.com/128/11918/11918414.png" featureContent='Modify your profile interactively by leveling up! :D and getting stuff from shop' featureTitle='Interactive profile!' />
             </div>
 
-            {/* Right Section */}
-            <div className="hidden md:flex flex-col ml-8 justify-center items-center relative">
-                <div className="elixirDiv right-20 mt-8">
-                    <XCard />
-                    <ElixirOfLife />
-                </div>
-                <ThemeToggleBtn />
-                <CodeLevelingLogo />
-            </div>
-        </div>
+        </>
     );
 }
 
